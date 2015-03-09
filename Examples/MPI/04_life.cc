ea6c01fa947d2875f5c5c474f68d8a96ea53b995
Danny Thorne <dannythorne@gmail.com>
2015-03-09 12:17:21 -0400

    update step almost done

--- Examples/MPI/04_life.cc
+++ Examples/MPI/04_life.cc
@@ -2,6 +2,13 @@
 #include <iostream>
 using namespace std;
 
+void update( char** u2d
+           , char** u2d_next
+           , int ibyte
+           , int i
+           , int j
+           , int sum );
+
 void display( char** u2d, int nibytes, int nj);
 
 int main()
@@ -20,6 +27,8 @@ int main()
   char** u2d;
   char* u1d_next;
   char** u2d_next;
+
+  char** utemp; // for pointer swap after each update
 
   u1d = new char[n];
   u2d = new char*[nj];
@@ -47,22 +56,79 @@ int main()
   display( u2d, nibytes, nj);
 
   int t, nt = 1;
+  int ibytep, ibytem;
+  int ip, im;
+  int jp, jm;
+  int sum;
 
   for( t=0; t<nt; t++)
   {
     for( j=0; j<nj; j++)
     {
+      jp = ( j + 1) % nj;
+      jm = ( j + nj - 1) % nj;
+
       for( ibyte=0; ibyte<nibytes; ibyte++)
       {
-        for( i=7; i>=0; i--)
+        ibytep = ( ibyte + 1) % nibytes;
+        ibytem = ( ibyte + nibytes - 1) % nibytes;
+
+        // msb (i=7) separately
+        i = 7;
+        ip = i - 1; // bit positions indexed backwards
+        sum = 0;
+        sum += 1&(u2d[j ][ibytem]); // lsb of ibytem
+        sum += 1&(u2d[j ][ibyte]>>ip);
+        sum += 1&(u2d[jm][ibytem]); // lsb of ibytem
+        sum += 1&(u2d[jm][ibyte]>>ip);
+        sum += 1&(u2d[jp][ibytem]); // lsb of ibytem
+        sum += 1&(u2d[jp][ibyte]>>ip);
+        sum += 1&(u2d[jm][ibyte]>>i );
+        sum += 1&(u2d[jp][ibyte]>>i );
+
+        update( u2d, u2d_next, ibyte, i, j, sum);
+
+        for( i=6; i>=1; i--)
         {
-          // TODO: u2d_next[][] = f( u2d[][]);
-          // where f is the GoL update rules
-          // (periodic boundaries)
+          // interior bits
+          ip = i - 1; // bit positions indexed backwards
+          im = i + 1; // bit positions indexed backwards
+
+          // update
+          sum = 0;
+          sum += 1&(u2d[j ][ibyte]>>im);
+          sum += 1&(u2d[j ][ibyte]>>ip);
+          sum += 1&(u2d[jm][ibyte]>>im);
+          sum += 1&(u2d[jm][ibyte]>>ip);
+          sum += 1&(u2d[jp][ibyte]>>im);
+          sum += 1&(u2d[jp][ibyte]>>ip);
+          sum += 1&(u2d[jm][ibyte]>>i );
+          sum += 1&(u2d[jp][ibyte]>>i );
+
+          update( u2d, u2d_next, ibyte, i, j, sum);
         }
+
+        // lsb (i=0) separately
+        i = 0;
+        im = i+1; // bit positions indexed backwards
+        sum = 0;
+        sum += 1&(u2d[j ][ibyte]>>im);
+        sum += 1&(u2d[j ][ibytep]>>7); // msb of ibytep
+        sum += 1&(u2d[jm][ibyte]>>im);
+        sum += 1&(u2d[jm][ibytep]>>7); // msb of ibytep
+        sum += 1&(u2d[jp][ibyte]>>im);
+        sum += 1&(u2d[jp][ibytep]>>7); // msb of ibytep
+        sum += 1&(u2d[jm][ibyte]>>i );
+        sum += 1&(u2d[jp][ibyte]>>i );
+        update( u2d, u2d_next, ibyte, i, j, sum);
+
       }
     }
-    // TODO: pointer swap
+
+    // pointer swap
+    utemp = u2d;
+    u2d = u2d_next;
+    u2d_next = utemp;
   }
 
   cout << endl << "Universe after " << nt << " update(s):" << endl;
@@ -74,6 +140,41 @@ int main()
   delete [] u1d;
 
   return 0;
+}
+
+void update( char** u2d
+           , char** u2d_next
+           , int ibyte
+           , int i
+           , int j
+           , int sum )
+{
+  if( 1&(u2d[j][ibyte]>>i))
+  {
+    // sum of 2 or 3 means leave it
+    // sum of less than 2 means it dies of isolation
+    // sum of > 3 means it dies of over crowding
+    if( sum < 2 || sum > 3)
+    {
+      u2d_next[j][ibyte] &= (~(1<<i));
+    }
+    else
+    {
+      // TODO: copy the 1
+    }
+  }
+  else
+  {
+    // sum of 3 means the cell gives birth
+    if( sum==3)
+    {
+      u2d_next[j][ibyte] |= (1<<i);
+    }
+    else
+    {
+      // TODO: copy the 0
+    }
+  }
 }
 
 void display( char** u2d, int nibytes, int nj)
