3db01e814ca3831fa3cf9062221c2c41ba07d317
Danny Thorne <dannythorne@gmail.com>
2015-03-13 21:47:29 -0400

    started tooling for domain padding

--- Examples/MPI/04_life.cc
+++ Examples/MPI/04_life.cc
@@ -9,7 +9,12 @@ void update( char** u2d
            , int j
            , int sum );
 
-void display( char** u2d, int nibytes, int nj);
+void display( char** u2d
+            , int nibytes
+            , int nj
+            , int ibyte0
+            , int j0
+            , int olap );
 
 int main()
 {
@@ -21,7 +26,15 @@ int main()
   if( ni%8) { ni = ni + 8 - ni%8;}
   nibytes = ni/8;
 
+  int olap = 8;
+  if( olap%8) { olap = olap + 8 - olap%8;} // enforce 8|olap
+
   int n = nibytes*nj;
+  int nnj = nj + 2*olap;
+  int nnibytes = nibytes + 2*olap/8;
+  int nn = nnibytes*nnj;
+  int ibyte0 = olap/8;
+  int j0 = olap;
 
   char* u1d;
   char** u2d;
@@ -30,22 +43,22 @@ int main()
 
   char** utemp; // for pointer swap after each update
 
-  u1d = new char[n];
-  u2d = new char*[nj];
-  u1d_next = new char[n];
-  u2d_next = new char*[nj];
+  u1d = new char[nn];
+  u2d = new char*[nnj];
+  u1d_next = new char[nn];
+  u2d_next = new char*[nnj];
 
   int i, ibyte, j;
 
-  for( j=0; j<nj; j++)
+  for( j=0; j<nnj; j++)
   {
-    u2d[j] = u1d + j*nibytes;
-    u2d_next[j] = u1d_next + j*nibytes;
+    u2d[j] = u1d + j*nibytes; // TODO
+    u2d_next[j] = u1d_next + j*nibytes; // TODO
   }
 
-  for( j=0; j<nj; j++)
+  for( j=0; j<nnj; j++)
   {
-    for( ibyte=0; ibyte<nibytes; ibyte++)
+    for( ibyte=0; ibyte<nnibytes; ibyte++)
     {
       u2d[j][ibyte] = 0; // j*nibytes + ibyte;
       u2d_next[j][ibyte] = 0;
@@ -53,14 +66,14 @@ int main()
   }
 
   // Initialize with glider pattern in top left corner.
-  u2d[3][/*ibyte*/0] |= 1<<6;
-  u2d[3][/*ibyte*/0] |= 1<<5;
-  u2d[3][/*ibyte*/0] |= 1<<4;
-  u2d[2][/*ibyte*/0] |= 1<<4;
-  u2d[1][/*ibyte*/0] |= 1<<5;
+  u2d[j0+3][ibyte0+/*ibyte*/0] |= 1<<6;
+  u2d[j0+3][ibyte0+/*ibyte*/0] |= 1<<5;
+  u2d[j0+3][ibyte0+/*ibyte*/0] |= 1<<4;
+  u2d[j0+2][ibyte0+/*ibyte*/0] |= 1<<4;
+  u2d[j0+1][ibyte0+/*ibyte*/0] |= 1<<5;
 
   cout << endl << "Universe seed:" << endl;
-  display( u2d, nibytes, nj);
+  display( u2d, nibytes, nj, ibyte0, j0, olap);
 
   int t, nt = 64;
   int ibytep, ibytem;
@@ -70,15 +83,15 @@ int main()
 
   for( t=0; t<nt; t++)
   {
-    for( j=0; j<nj; j++)
+    for( j=j0; j<j0+nj; j++)
     {
-      jp = ( j + 1) % nj;
-      jm = ( j + nj - 1) % nj;
+      jp = ( j-j0 + 1) % nj + j0;
+      jm = ( j-j0 + nj - 1) % nj + j0;
 
-      for( ibyte=0; ibyte<nibytes; ibyte++)
+      for( ibyte=ibyte0; ibyte<ibyte0+nibytes; ibyte++)
       {
-        ibytep = ( ibyte + 1) % nibytes;
-        ibytem = ( ibyte + nibytes - 1) % nibytes;
+        ibytep = ( ibyte-ibyte0 + 1) % nibytes + ibyte0;
+        ibytem = ( ibyte-ibyte0 + nibytes - 1) % nibytes + ibyte0;
 
         // msb (i=7) separately
         i = 7;
@@ -142,7 +155,7 @@ int main()
   } // for( t=0; t<nt; t++)
 
   cout << endl << "Universe after " << nt << " update(s):" << endl;
-  display( u2d, nibytes, nj);
+  display( u2d, nibytes, nj, ibyte0, j0, olap);
 
   delete [] u2d_next;
   delete [] u1d_next;
@@ -189,13 +202,18 @@ void update( char** u2d
   }
 }
 
-void display( char** u2d, int nibytes, int nj)
+void display( char** u2d
+            , int nibytes
+            , int nj
+            , int ibyte0
+            , int j0
+            , int olap )
 {
   int i, j, ibyte;
 
-  for( j=0; j<nj; j++)
+  for( j=j0; j<j0+nj; j++)
   {
-    for( ibyte=0; ibyte<nibytes; ibyte++)
+    for( ibyte=ibyte0; ibyte<ibyte0+nibytes; ibyte++)
     {
       for( i=7; i>=0; i--)
       {
